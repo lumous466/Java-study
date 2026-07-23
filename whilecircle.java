bfaa6bd08748835cd34dd439a890b2d574189616
lumous466 <nkg8rztcsg@privaterelay.appleid.com>
2026-07-23 17:14:47 +0800

    Create whilecircle.java

---
+++ whilecircle.java
@@ -0,0 +1,15 @@
+public class whilecircle{
+  public static void mian(String[]args){
+int x = 12345;
+int temp = x;
+int num = 0;
+while(x != 0){
+  int ge = x % 10;//从左到右获取每个数字
+  x = x/10;
+  num = num*10 + ge;
+}
+System.out.println(num);
+System.out.println(x);
+System.out.println(num == temp);
+  }
+}
